2f2a41c79931150723f5688c9b8d12913beec985
Koushik M <120298961+KoushikMCN@users.noreply.github.com>
2024-03-15 14:05:54 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -6,12 +6,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCg9iDyVNbp2ehcb3OkEwOzflTGOqg68Ao",
-  authDomain: "dinein-7480a.firebaseapp.com",
-  projectId: "dinein-7480a",
-  storageBucket: "dinein-7480a.appspot.com",
-  messagingSenderId: "178739097515",
-  appId: "1:178739097515:web:43242b38c34f99fd0eecee"
+  apiKey: "xyz",
+  authDomain: ".com",
+  projectId: "dinein",
+  storageBucket: "dinein",
+  messagingSenderId: "129",
+  appId: "1:129:web:w34e"
 };
 
 // Initialize Firebase
